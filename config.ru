1dacb10396d12bdb88d926a6a4871950a9d3be2b
blancopado <pablo.dominguezblanco@gmail.com>
2015-10-27 17:38:10 +0100

    Chapter 18: index.erb

--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
 require 'rubygems'
-require File.join(File.dirname(__FILE__), 'lib/battle.rb')
+require File.join(File.dirname(__FILE__), 'app.rb')
 
 run Battle
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,8 +1,10 @@
+require 'spec_helper'
+
 feature "Enter names" do
   scenario "Submitting Name1 Name2" do
       visit('/')
-      fill_in('Name1', with: 'Rob')
-      fill_in('Name2', with: 'Pablo')
+      fill_in :Name1, with: 'Rob'
+      fill_in :Name2, with: 'Pablo'
       click_button 'Submit'
       expect(page).to have_content 'Rob vs. Pablo'
     end
