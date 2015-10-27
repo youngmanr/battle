8789a78e1fe192c0fd20e34f9d5ef82e48c52b39
Robert Youngman <rob_youngman@yahoo.com>
2015-10-27 17:01:59 +0000

    Chpt.18 push

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  
+
 get '/' do
   erb(:index)
+end
+
+post '/names' do
+  @player_1_name = params[:player_1_name]
+  @player_2_name = params[:player_2_name]
+  erb:play
 end
 
    #start the server if ruby file executed directly
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 feature "Enter names" do
   scenario "Submitting Name1 Name2" do
       visit('/')
-      fill_in :Name1, with: 'Rob'
-      fill_in :Name2, with: 'Pablo'
+      fill_in :player_1_name, with: 'Rob'
+      fill_in :player_2_name, with: 'Pablo'
       click_button 'Submit'
       expect(page).to have_content 'Rob vs. Pablo'
     end
