18af0bcb3e1418e51891a8efdabfe3eb6c1e3ba8
blancopado <pablo.dominguezblanco@gmail.com>
2015-10-27 18:50:56 +0100

    Challenge 19

--- app.rb
+++ app.rb
@@ -2,14 +2,22 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+enable :sessions
+
 get '/' do
   erb(:index)
 end
 
 post '/names' do
+  session[:player_1_name] = params[:player_1_name]
+  session[:player_2_name] = params[:player_2_name]
+  redirect '/play'
+end
+
+get '/play' do
   @player_1_name = params[:player_1_name]
   @player_2_name = params[:player_2_name]
-  erb:play
+  erb :play
 end
 
    #start the server if ruby file executed directly
